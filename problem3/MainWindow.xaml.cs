4136a946ad23fd2a2cffcd3f73c83a671a26424e
angat-yogi <angat.yogi77@gmail.com>
2021-10-12 16:39:45 -0500

    problem3 Completed

---
+++ problem3/MainWindow.xaml.cs
@@ -0,0 +1,104 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace problem3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        RandomVideo video;
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            btnPlayPause.IsEnabled = false;
+            btnNext.IsEnabled = false;
+            btnStop.IsEnabled = false;
+            videoframe.LoadedBehavior = MediaState.Manual;
+
+        }
+
+        private void btnPlayPause_Click(object sender, RoutedEventArgs e)
+        {
+            string content = btnPlayPause.Content.ToString().ToLower();
+
+            switch (content)
+            {
+                case "play":
+                    videoframe.Play();
+                    btnPlayPause.Content = "Pause";
+                    break;
+                case "pause":
+                    videoframe.Pause();
+                    btnPlayPause.Content = "Play";
+                    break;
+            }
+
+        }
+
+        private void Get_Random_Video_Click(object sender, RoutedEventArgs e)
+        {
+            using (var client = new HttpClient())
+            {
+
+                var api = client.GetAsync("http://pcbstuou.w27.wh-2.com/webservices/3033/api/random/video").Result;
+
+                if (api.IsSuccessStatusCode)
+                {
+
+                    video = JsonConvert.DeserializeObject<RandomVideo>(api.Content.ReadAsStringAsync().Result);
+                }
+            }
+
+            Uri url = new Uri(video.url.ToString());
+            videoframe.Source = url;
+            btnPlayPause.IsEnabled = true;
+            btnNext.IsEnabled = true;
+            btnStop.IsEnabled = true;
+        }
+
+        private void btnStop_Click(object sender, RoutedEventArgs e)
+        {
+            videoframe.Stop();
+            btnPlayPause.Content = "Play";
+        }
+
+        private void btnNext_Click(object sender, RoutedEventArgs e)
+        {
+            using (var client = new HttpClient())
+            {
+
+                var api = client.GetAsync("http://pcbstuou.w27.wh-2.com/webservices/3033/api/random/video").Result;
+
+                if (api.IsSuccessStatusCode)
+                {
+
+                    video = JsonConvert.DeserializeObject<RandomVideo>(api.Content.ReadAsStringAsync().Result);
+                }
+            }
+
+            Uri url = new Uri(video.url.ToString());
+            videoframe.Source = url;
+            videoframe.Pause();
+            btnPlayPause.Content = "Play";
+
+        }
+    }
+}
---
+++ problem3/RandomVideo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace problem3
+{
+    public class RandomVideo
+    {
+        public string description { get; set; }
+        public string url { get; set; }
+        public int id { get; set; }
+
+    }
+}
